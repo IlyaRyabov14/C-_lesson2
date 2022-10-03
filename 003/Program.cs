43fb9a7d604092e7150b35876b81d019022220bf
Ryaba1488 <ilya-ryabov03@yandex.ru>
2022-10-03 18:39:42 +0300

    003

---
+++ 003/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {18,27,36,45,54,63,72,81,45};
+
+int n = array.Length;
+int find  = 45;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
